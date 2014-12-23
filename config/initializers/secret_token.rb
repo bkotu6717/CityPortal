3a39103fae277afa8edd252de7a033a420932117
Bhaskar <bhaskar.kotu@coupa.com>
2014-12-24 00:30:59 +0530

    Adding devise for authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CityPortal::Application.config.secret_token = '38441cf0330236f6a3a453e28b7a85c5bd7fe2258d79768aea5d8ee99470d9474396c50e1b0515d5c74674d884a621048d0f65954102cc640f78266da583d645'
